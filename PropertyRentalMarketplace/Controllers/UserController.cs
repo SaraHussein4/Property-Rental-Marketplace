792de2face31090475958cf6e017ee4a935f505d
SaraHussein4 <saraomran12002@gmail.com>
2025-04-20 14:57:08 +0200

    add UserController

---
+++ PropertyRentalMarketplace/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyRentalMarketplace.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
