4202ce62074fc9fe918850a007ff536f373a40db
SaraHussein4 <saraomran12002@gmail.com>
2025-04-20 17:27:22 +0200

    add PropertyRepo

---
+++ PropertyRentalBL/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,15 @@
+﻿using PropertyBL.Interfaces;
+using PropertyBL.Repositories;
+using PropertyRentalDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyRentalBL.Interfaces
+{
+    public interface IPropertyRepository : IGenericRepository<Property>
+    {
+    }
+}
