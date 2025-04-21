c63f5674bc56e836640421311ad5da8c51972117
MostafaMuradA <moustafa.m.adam@gmail.com>
2025-04-21 23:30:30 +0200

    Login and Signup

---
+++ PropertyRentalMarketplace/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyRentalMarketplace.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PhoneLogin()
+        {
+            return View();
+        }
+        public IActionResult EmailLogin()
+        {
+            return View();
+        }
+        public IActionResult EnterPassword()
+        {
+            return View();
+        }
+        public IActionResult SetPassword()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
