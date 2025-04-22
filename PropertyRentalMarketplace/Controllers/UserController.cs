441413ef0702176bf155ab7f712df96a21d44c52
SaraHussein4 <saraomran12002@gmail.com>
2025-04-22 22:17:28 +0200

    add user style

--- PropertyRentalMarketplace/Controllers/UserController.cs
+++ PropertyRentalMarketplace/Controllers/UserController.cs
@@ -16,7 +16,6 @@ namespace PropertyRentalMarketplace.Controllers
     [Authorize]
     public class UserController : Controller
     {
-        // IUserRepository
         private readonly IPropertyRepository _propertyRepository;
         private readonly IFavouriteRepository _favouriteRepository;
         public UserController(IPropertyRepository propertyRepository, IFavouriteRepository favouriteRepository)
@@ -26,56 +25,8 @@ namespace PropertyRentalMarketplace.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var images = new List<Image>
-            {
-                new Image { Id = 1, Path = "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 1 },
-                new Image { Id = 2, Path= "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 1 },
-                new Image { Id = 2, Path= "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 1 },
-                new Image { Id = 2, Path= "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 1 },
-             
-            };
-            var images2 = new List<Image>
-            {
-               new Image { Id = 2, Path= "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 2},
-                new Image { Id = 2, Path= "https://i.pinimg.com/736x/c6/be/a0/c6bea0070d17659af2f8856e4a627e6c.jpg", PropertyId = 2}
-            };
-            var property = new PropertyViewModel()
-                {
-                    Id = 1,
-                    Name = "Name",
-                    Address = "LLL",
-                    BedRooms = 3,
-                    BathRooms = 2,
-                    BetsAllowd = 0,
-                    GarageSlots = 1,
-                    IsListed = true,
-                    IsFeatured = false,
-                    ListedAt = DateTime.Now,
-                    UnListDate = (DateTime.Now).AddDays(12),
-                    ListingType = ListingType.Rent,
-                    Images=images
-
-                };
-            var property1 = new PropertyViewModel()
-            {
-                Id = 2,
-                Name = "Name",
-                Address = "LLL",
-                BedRooms = 3,
-                BathRooms = 2,
-                BetsAllowd = 0,
-                GarageSlots = 1,
-                IsListed = true,
-                IsFeatured = true,
-                ListedAt = DateTime.Now,
-                UnListDate = (DateTime.Now).AddDays(12),
-                ListingType = ListingType.Rent,
-                Images = images2
-            };
-
-
-            var allProperities = new List<PropertyViewModel> { property ,property1};
-            var featuedModel = allProperities.Where(model=>model.IsFeatured==true).ToList();
+            var allProperities = (await _propertyRepository.GetAll()).OrderByDescending(p=>p.ListedAt).Take(4).ToList();
+            var featuedModel = await _propertyRepository.GetAllFeatured();
             var model = new PropertyPageViewModel
             {
                 AllProperities = allProperities,
