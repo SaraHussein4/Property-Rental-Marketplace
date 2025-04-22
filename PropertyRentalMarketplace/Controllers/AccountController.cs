78e88c38dac32453b29daae6e424be3e6e6464b6
Aml A Shabana <amlshbana28@gmail.com>
2025-04-22 02:04:52 +0200

    login without design

--- PropertyRentalMarketplace/Controllers/AccountController.cs
+++ PropertyRentalMarketplace/Controllers/AccountController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+using PropertyDAL.Models;
+using PropertyRentalMarketplace.ViewModels;
 
 namespace PropertyRentalMarketplace.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager ,
+            SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
 
         public IActionResult Index()
         {
@@ -32,6 +45,36 @@ namespace PropertyRentalMarketplace.Controllers
         public IActionResult Register()
 
         {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid) { 
+              User userFromDb = await userManager.FindByNameAsync(loginViewModel.UserName);
+                if (userFromDb != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(userFromDb, loginViewModel.Password);
+
+                    if (found)
+                    {
+                        //create cookie
+                        await signInManager.SignInAsync(userFromDb, loginViewModel.RememberMe);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+                ModelState.AddModelError("","Error login");
+            }
+            return View("Login",loginViewModel);
+        }
+        public IActionResult Logout()
+        { 
             return View();
         }
     }
