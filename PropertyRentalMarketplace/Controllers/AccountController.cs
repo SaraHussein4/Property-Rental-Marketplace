dc654ba1ac6385cb08c180305359f94e182d4ecf
SaraHussein4 <saraomran12002@gmail.com>
2025-04-22 02:36:44 +0200

    update user model

--- PropertyRentalMarketplace/Controllers/AccountController.cs
+++ PropertyRentalMarketplace/Controllers/AccountController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PropertyDAL.Models;
+using PropertyRentalMarketplace.ViewModels;
 
 namespace PropertyRentalMarketplace.Controllers
 {
     public class AccountController : Controller
     {
-<<<<<<< HEAD
+
+        [HttpGet]
         public IActionResult Register()
-=======
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) {
+                var User = new User()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    IsAgree = model.IsAgree,
+                    Gender = model.Gender
+                };
+            }
+            return View();
+        }
+
+
         public IActionResult Index()
         {
             return View();
@@ -28,7 +51,6 @@ namespace PropertyRentalMarketplace.Controllers
             return View();
         }
         public IActionResult ForgotPassword()
->>>>>>> c63f5674bc56e836640421311ad5da8c51972117
         {
             return View();
         }
