c5dc8f633d70b4dd7d38171ec8e50d7b5e725db6
SaraHussein4 <saraomran12002@gmail.com>
2025-05-07 00:33:00 +0300

    External Register

--- PropertyRentalMarketplace/Controllers/AccountController.cs
+++ PropertyRentalMarketplace/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using PropertyDAL.Models;
 using PropertyRentalDAL.Models;
 using PropertyRentalMarketplace.Helpers;
 using PropertyRentalMarketplace.ViewModels;
+using System.Security.Claims;
 
 namespace PropertyRentalMarketplace.Controllers
 {
@@ -94,6 +95,105 @@ namespace PropertyRentalMarketplace.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult ExternalRegister(string provider, string returnUrl = null, string role = null)
+        {
+            var allowedRoles = new[] { AppRoles.Host, AppRoles.User };
+            role = allowedRoles.Contains(role) ? role : AppRoles.User;
+
+            var redirectUrl = Url.Action("ExternalRegisterCallback", "Account", new { returnUrl });
+
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+
+            properties.Items["role"] = role;
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                properties.Items["returnUrl"] = returnUrl;
+            }
+
+            return Challenge(properties, provider);
+        }
+        public async Task<IActionResult> ExternalRegisterCallback(string remoteError = null)
+        {
+            if (remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error from external provider: {remoteError}");
+                return RedirectToAction("Register");
+            }
+
+            var info = await signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+                return RedirectToAction("Register");
+
+            var allowedRoles = new[] { AppRoles.Host, AppRoles.User };
+            var role = info.AuthenticationProperties.Items.TryGetValue("role", out var extractedRole)
+                && allowedRoles.Contains(extractedRole)
+                    ? extractedRole
+                    : AppRoles.User;
+
+            var returnUrl = info.AuthenticationProperties.Items.TryGetValue("returnUrl", out var extractedReturnUrl)
+                ? extractedReturnUrl
+                : null;
+
+            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+            var name = info.Principal.FindFirstValue(ClaimTypes.Name);
+
+            var existingUser = await userManager.FindByEmailAsync(email);
+            if (existingUser != null)
+            {
+                await signInManager.SignInAsync(existingUser, isPersistent: false);
+
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    returnUrl = role == AppRoles.Host
+                        ? Url.Action("Index", "Host")
+                        : Url.Action("Index", "User");
+                }
+
+                return LocalRedirect(returnUrl);
+            }
+
+            var newUser = new User
+            {
+                UserName = email.Split('@')[0],
+                Email = email,
+                Name = name,
+                IsAgree = true
+            };
+
+            var createResult = await userManager.CreateAsync(newUser);
+            if (createResult.Succeeded)
+            {
+                await userManager.AddLoginAsync(newUser, info);
+
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+
+                await userManager.AddToRoleAsync(newUser, role);
+                await signInManager.SignInAsync(newUser, isPersistent: false);
+
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    var roles = await userManager.GetRolesAsync(newUser);
+                    if (roles.Contains(AppRoles.Admin))
+                        returnUrl = Url.Action("Index", "Admin");
+                    else if (roles.Contains(AppRoles.Host))
+                        returnUrl = Url.Action("Index", "Host");
+                    else
+                        returnUrl = Url.Action("Index", "User");
+                }
+
+                return LocalRedirect(returnUrl);
+            }
+
+            foreach (var error in createResult.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return RedirectToAction("Index", "User");
+        }
+
+
 
         [HttpGet]
         public IActionResult Login()
